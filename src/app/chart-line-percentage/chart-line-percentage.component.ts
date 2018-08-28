d957932eef0d15063f68a2b9380458fdf3602d05
Voltaire Bazurto Blacio <vbazurtob@gmail.com>
2018-08-28 04:14:51 +0000

    Fix for angular 6 and .gitignore added

--- src/app/chart-line-percentage/chart-line-percentage.component.ts
+++ src/app/chart-line-percentage/chart-line-percentage.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ChartLinePercentageComponent implements OnInit {
 
 @Input()
-private percentageCovered: number;
+public percentageCovered: number;
 private percentageNotCovered: number;
 
   constructor() { 
